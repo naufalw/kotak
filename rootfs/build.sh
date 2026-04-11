91e1ff03f2b2ffdf2e5c8ee533c4824d14210ecb
naufalw <naufalwiwitputra@outlook.com>
2026-04-11 23:36:42 +1000

    feat: bundle kotak-guest bin and make startup service to rootfs

--- rootfs/build.sh
+++ rootfs/build.sh
@@ -42,12 +42,49 @@ docker run --rm -v $MOUNT:/my-rootfs alpine sh -c '
     for d in bin etc lib root sbin usr; do tar c "/$d" | tar x -C /my-rootfs; done
     for dir in dev proc run sys var; do mkdir -p /my-rootfs/${dir}; done
     mkdir -p /my-rootfs/var/empty
+    mkdir -p /my-rootfs/var/log
 '
+
+echo "==> install kotak-guest"
+sudo tee $MOUNT/etc/init.d/kotak-guest > /dev/null << 'EOF'
+#!/sbin/openrc-run
+
+name="kotak-guest"
+description="Kotak guest agent"
+command="/usr/local/bin/kotak-guest"
+command_background=true
+pidfile="/run/kotak-guest.pid"
+output_log="/var/log/kotak-guest.log"
+error_log="/var/log/kotak-guest.log"
+
+depend() {
+    after sysfs devfs modules
+}
+
+start_pre() {
+    local i=0
+    while [ ! -e /dev/vsock ] && [ $i -lt 10 ]; do
+        sleep 0.5
+        i=$((i+1))
+    done
+    if [ ! -e /dev/vsock ]; then
+        eerror "/dev/vsock not found"
+        return 1
+    fi
+}
+EOF
+
+sudo chmod +x $MOUNT/etc/init.d/kotak-guest
+sudo ln -s /etc/init.d/kotak-guest "$MOUNT/etc/runlevels/default/kotak-guest"
+
+echo "==> copy guest bin"
+sudo cp "/home/naufal/kotak/target/x86_64-unknown-linux-musl/release/kotak-guest" $MOUNT/usr/local/bin/kotak-guest
+sudo chmod +x $MOUNT/usr/local/bin/kotak-guest
 
 echo "==> unmount"
 sudo umount $MOUNT
 
-echo "==> copyy"
+echo "==> copyy rootfs to firecracker-local"
 cp $ROOTFS $DEST
 
 echo "==> done! rootfs here: $DEST"
