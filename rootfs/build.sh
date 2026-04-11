337fd0140e29425d9653c0ea1099212fef05df80
naufalw <naufalwiwitputra@outlook.com>
2026-04-11 21:32:31 +1000

    feat: refactor rootfs build script and add dns by default

--- rootfs/build.sh
+++ rootfs/build.sh
@@ -17,20 +17,28 @@ echo "==> populating rootfs"
 docker run --rm -v $MOUNT:/my-rootfs alpine sh -c '
     apk add openrc util-linux openssh bash
 
+    # Serial Console
     ln -s agetty /etc/init.d/agetty.ttyS0
     echo ttyS0 > /etc/securetty
     rc-update add agetty.ttyS0 default
 
+    # Boot services
     rc-update add devfs boot
     rc-update add procfs boot
     rc-update add sysfs boot
-    rc-update add sshd default
 
+    # SSH services
+    rc-update add sshd default
     echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
     echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
     echo "root:root" | chpasswd
     ssh-keygen -A
 
+    # DNS
+    echo "nameserver 8.8.8.8" > /etc/resolv.conf
+    echo "hosts: files dns" > /etc/nsswitch.conf
+
+    # Filesystem copy
     for d in bin etc lib root sbin usr; do tar c "/$d" | tar x -C /my-rootfs; done
     for dir in dev proc run sys var; do mkdir -p /my-rootfs/${dir}; done
     mkdir -p /my-rootfs/var/empty
