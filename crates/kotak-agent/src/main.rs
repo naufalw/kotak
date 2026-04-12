1cdee006848dcae563d1e681b4973c463ac375dd
naufalw <naufalwiwitputra@outlook.com>
2026-04-12 21:54:56 +1000

    feat: use http server in main, remove testing code

--- crates/kotak-agent/src/main.rs
+++ crates/kotak-agent/src/main.rs
@@ -1,48 +1,33 @@
+use std::{collections::HashMap, sync::Arc};
+
 use anyhow::Result;
 use kotak_agent::{
-    filesystem::FilesystemManager,
+    api::{AppState, router},
     network::IpamAllocator,
-    sandbox::{Sandbox, SandboxConfig, resume},
+    sandbox::SandboxConfig,
     snapshot::SnapshotStore,
 };
+use tokio::sync::Mutex;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
-    let id = "sandbox-001";
-    let ipam = IpamAllocator::new();
-    let store = SnapshotStore::new();
-    let fs = FilesystemManager::new("/home/naufal/kotak/firecracker-local/rootfs.ext4");
+    let state = Arc::new(AppState {
+        sandboxes: Mutex::new(HashMap::new()),
+        ipam: IpamAllocator::new(),
+        store: SnapshotStore::new(),
+        config: SandboxConfig {
+            kernel_path: "/home/naufal/kotak/firecracker-local/vmlinux-6.1.155.bin".to_string(),
+            guest_cid: 3,
+        },
+        base_rootfs: "/home/naufal/kotak/firecracker-local/rootfs.ext4".to_string(),
+    });
 
-    let config = SandboxConfig {
-        kernel_path: "/home/naufal/kotak/firecracker-local/vmlinux-6.1.155.bin".to_string(),
-        mac: "AA:FC:00:00:00:01".to_string(),
-        guest_cid: 3,
-    };
-
-    let sandbox = Sandbox::create(id, &ipam, fs, &config).await?;
-    let response = sandbox.exec("uname -a").await?;
-    tracing::info!("stdout: {}", response.stdout);
-
-    let response = sandbox
-        .exec("echo 'kotak works' > /root/testfile.txt && cat /root/testfile.txt")
-        .await?;
-    tracing::info!("created: {}", response.stdout.trim());
-
-    sandbox.hibernate(&store).await?;
-    sandbox.destroy(&ipam).await?;
-
-    tracing::info!("here");
-    let fs = FilesystemManager::new("/home/naufal/kotak/firecracker-local/rootfs.ext4");
-    let sandbox = resume(id, &ipam, fs, &store, &config).await?;
-    let response = sandbox.exec("uname -a").await?;
-    tracing::info!("STDOUTT HERE: {}", response.stdout);
-
-    let response = sandbox.exec("cat /root/testfile.txt").await?;
-    tracing::info!("HEREEEEEE: {}", response.stdout.trim());
-
-    sandbox.destroy(&ipam).await?;
+    let app = router(state);
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    tracing::info!("here AT 0.0.0.0:3000");
+    axum::serve(listener, app).await?;
 
     Ok(())
 }
